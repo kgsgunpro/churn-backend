d715de34f557a5e7f62e8d0c7003a96570b8f11e
K GUNASEKHAR <142863424+kgsgunpro@users.noreply.github.com>
2026-01-04 14:54:38 +0000

    second commit

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 age = 25
 salary = 45000.5
 is_active = True
-print(age, salary, is_active)
+tenure = 10
+def churn_risk(tenure):
+    if tenure < 12:
+        return "High risk"
+    else:
+        return "Low risk"
+print(age, salary, is_active ,churn_risk(tenure))
